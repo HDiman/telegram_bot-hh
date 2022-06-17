3823c6b4df3784a8e046809ba66ee7a48b9c8183
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-17 19:49:36 +0300

    Program is ready for use. Last commit before loading to server.

--- main.py
+++ main.py
@@ -4,25 +4,44 @@ import telebot
 from auth_data import token
 
 
-url_1 = "https://hh.ru/search/vacancy?text=&fromSearchLine=true&area=1"
-url_2 = "https://hh.ru/search/vacancy?text=&fromSearchLine=true&area=1&page=1&hhtmFrom=vacancy_search_list"
-url_3 = "https://hh.ru/search/vacancy?text=&fromSearchLine=true&area=1&page=2&hhtmFrom=vacancy_search_list"
-url_4 = "https://hh.ru/search/vacancy?text=parsing&from=suggest_post&salary=&clusters=true&area=1&ored_clusters=true&enable_snippets=true"
-url_parsing = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&ored_clusters=true&text=парсинг&search_period=7&hhtmFrom=vacancy_search_list"
+def get_data():
+    url_parsing = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&ored_clusters=true&text=парсинг&search_period=1&hhtmFrom=vacancy_search_list"
+    headers = {
+        "Accept": "*/*",
+        "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+    }
 
-headers = {
-    "Accept": "*/*",
-    "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
-}
+    # --- Block to get data ---
+    req = requests.get(url=url_parsing, headers=headers)
+    response = req.text
 
-# --- Block for use only ones ---
-# req = requests.get(url=url_parsing, headers=headers)
-# response = req.text
-# # print(response)
-#
-# with open("index.html", "w") as file:
-#     file.write(response)
+    soup = BeautifulSoup(response, "lxml")
+    all_title = soup.find_all(class_="g-user-content")
+    return all_title
 
 
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Code to solve problems")
+
+    @bot.message_handler(content_types=['text'])
+    def send_text(message):
+        if message.text.lower() == 'hh':
+            try:
+                all_title = get_data()
+                for title in all_title:
+                    bot.send_message(message.chat.id, title.text)
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Disconnection!!!")
+        else:
+            bot.send_message(message.chat.id, "More coding")
+
+    bot.polling()
 
 
+if __name__ == "__main__":
+    telegram_bot(token=token)
--- playground.py
+++ playground.py
@@ -30,3 +30,40 @@ def telegram_bot(token):
 
 if __name__ == "__main__":
     get_data()
+
+
+# --- Block -- last code
+import requests
+from bs4 import BeautifulSoup
+import telebot
+from auth_data import token
+
+
+# url_1 = "https://hh.ru/search/vacancy?text=&fromSearchLine=true&area=1" #area=1 - Moscow
+# url_2 = "https://hh.ru/search/vacancy?text=&fromSearchLine=true&area=1&page=1&hhtmFrom=vacancy_search_list"
+# url_3 = "https://hh.ru/search/vacancy?text=&fromSearchLine=true&area=1&page=2&hhtmFrom=vacancy_search_list"
+# url_4 = "https://hh.ru/search/vacancy?text=parsing&from=suggest_post&salary=&clusters=true&area=1&ored_clusters=true&enable_snippets=true"
+# url_parsing = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&ored_clusters=true&text=парсинг&search_period=7&hhtmFrom=vacancy_search_list"
+#
+# headers = {
+#     "Accept": "*/*",
+#     "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+# }
+
+# --- Block for use only ones ---
+# req = requests.get(url=url_parsing, headers=headers)
+# response = req.text
+# # print(response)
+#
+# with open("index.html", "w") as file:
+#     file.write(response)
+
+with open("index.html", "r") as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, "lxml")
+
+all_title = soup.find_all(class_="g-user-content")
+for title in all_title:
+    print(title.text)
+
